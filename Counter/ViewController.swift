3de7959c2aaf011e26b7210f1c615de50f1d20a3
ArtemChalkov <F9152005012@yandex.ru>
2023-05-17 15:57:01 +0300

    Изменен вывод в консоль в файле AppDelegate

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBAction func plusButton(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
