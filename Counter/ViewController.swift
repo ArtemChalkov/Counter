d9f2e6e6d5a77319ec429559354122ef09138005
ArtemChalkov <F9152005012@yandex.ru>
2023-05-17 17:16:59 +0300

    Изменен вывод в консоль в файле AppDelegate

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,16 +7,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-    @IBOutlet weak var label: UILabel!
-    @IBAction func plusButton(_ sender: Any) {
-    }
+    var history: String = "История изменений"
+    
+    private var count: Int = 0
+    
+    @IBOutlet weak var countLabel: UILabel!
+    
+    @IBOutlet weak var plusButton: UIButton!
+
+    @IBOutlet weak var minusButton: UIButton!
+    
+    @IBOutlet weak var historyTextView: UITextView!
+    
+    @IBOutlet weak var nulButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        historyTextView.text = history
     }
+    
 
-
+    @IBAction func countButtonTapped(_ sender: UIButton) {
+        
+        let currentDate = Date()
+        
+        switch sender {
+            
+        case plusButton:
+            count += 1
+           // «[дата и время]: значение изменено на -1/+1»
+            
+            history += "\n" + "\(currentDate) значение изменено на +1: "
+            
+        case minusButton:
+            count -= 1
+            
+            history += "\n" + "\(currentDate) значение изменено на -1: "
+            
+        case nulButton:
+            count = 0
+            history += "\n" + "\(currentDate) значение сброшено: "
+            
+        default: break
+        }
+        
+      
+        
+        countLabel.text = "Значение счетчика \(count)"
+        historyTextView.text = history
+    }
+    
 }
 
